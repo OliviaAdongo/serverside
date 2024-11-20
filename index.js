00af2afee7626e56f8a3dfcef53b61e273e26f43
Olivia Adongo <akinyioliviatove@gmail.com>
2024-11-21 01:06:25 +0300

    Milestone: Listening to server and testing local host get crud

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("Hello")
+const express = require("express");
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server listening on port 3000");
+});
+app.get("/", (req, res) => {
+  res.send("Hello from SolidRoots NodeAPI");
+});
