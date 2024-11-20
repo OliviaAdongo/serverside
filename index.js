e22b3d1434060ae93f479d2f4703fafb43aaa92a
Olivia Adongo <akinyioliviatove@gmail.com>
2024-11-21 02:21:13 +0300

    Milestone: Mongodb setup

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require("express");
+const mongoose = require('mongoose');
 const app = express();
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000");
 });
 app.get("/", (req, res) => {
-  res.send("Hello from SolidRoots NodeAPI");
+  res.send("Hello from SolidRoots NodeAPI server updated");
 });
+
+
+
