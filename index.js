3435b6b09878aed174d011d0f1f5ef18d7202254
Olivia Adongo <akinyioliviatove@gmail.com>
2024-11-21 04:08:07 +0300

    Milestone: Delete api

--- index.js
+++ index.js
@@ -50,7 +50,21 @@ app.put("/api/product/:id", async (req, res) => {
     }
     const updatedProduct = await Product.findById(id);
     res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+// DELETE  PRODUCT
 
+app.delete("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndDelete(id);
+
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    res.status(200).json({ message: "Product deleted successfully" });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
