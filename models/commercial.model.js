b7f3fdc50515e12306044c624ecf445956394e4e
Olivia Adongo <akinyioliviatove@gmail.com>
2024-11-29 09:23:16 +0300

    Milestone: Form and model updated for land

--- models/commercial.model.js
+++ models/commercial.model.js
@@ -2,12 +2,19 @@ const mongoose = require("mongoose");
 
 const commercialSchema = new mongoose.Schema(
   {
+    images: {
+      type: [String],
+      validate: {
+        validator: (arr) => arr.length >= 1 && arr.length <= 10,
+        message: "You must provide between 1 and 10 images.",
+      },
+    },
     location: { type: String, required: true },
     price: { type: String, required: true },
     availability: {
       type: String,
       enum: ["Available", "Not Available"],
-      default: "available",
+      default: "Available",
     },
     category: {
       type: String,
@@ -22,7 +29,14 @@ const commercialSchema = new mongoose.Schema(
     description: {
       type: String,
       required: true,
-    }
+    },
+    floorplan: {
+      type: [String],
+      validate: {
+        validator: (arr) => arr.length >= 1 && arr.length <= 10,
+        message: "You must provide between 1 and 10 images.",
+      },
+    },
   },
   { timestamps: true }
 );
